efb563ec6809592e6212944b782bfe4f37826dc6
nehach028 <neha.chaturvedi028@gmail.com>
2018-06-12 12:35:45 +0000

    Exercise1Controlled Components

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state={
+  text:''
+  }
+updateState=(textValue)=>{
+	this.setState(()=>({text:textValue}))
+  	//this.setState(()=>({query:query.trim()}))
+}  
+render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input type="text" placeholder="Say Something" value={this.state.text} onChange={(event)=>{this.updateState(event.target.value)}}/>
+          <p className="echo">Echo:{this.state.text}</p>
+          <p>This should mirror the text you typed into the input field.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
